bf5e8c21baeec583bfa60d31f7870942c121ff4b
Iftikhar Ali Wazir <94034285+iftikh36@users.noreply.github.com>
2023-08-20 02:41:23 +0530

    Update chat_screen.dart

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,7 +1,10 @@
+import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 import 'chatmessage.dart';
+import 'threedots.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -12,15 +15,71 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _controller = TextEditingController();
-  final List<ChatMessage> _message = [];
+  final List<ChatMessage> _messages = [];
+  late OpenAI? chatGPT;
+  bool _isImageSearch = false;
 
-  void _sendMessage() {
-    ChatMessage _message = ChatMessage(text: _controller.text, sender: "user");
+  bool _isTyping = false;
+
+  @override
+  void initState() {
+    chatGPT = OpenAI.instance.build(
+        token: dotenv.env["API_KEY"],
+        baseOption: HttpSetup(receiveTimeout: 60000));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    chatGPT?.close();
+    chatGPT?.genImgClose();
+    super.dispose();
+  }
+
+  // Link for api - https://beta.openai.com/account/api-keys
+
+  void _sendMessage() async {
+    if (_controller.text.isEmpty) return;
+    ChatMessage message = ChatMessage(
+      text: _controller.text,
+      sender: "user",
+      isImage: false,
+    );
+
     setState(() {
-      _message.insert(0, _message);
+      _messages.insert(0, message);
+      _isTyping = true;
     });
 
     _controller.clear();
+
+    if (_isImageSearch) {
+      final request = GenerateImage(message.text, 1, size: "256x256");
+
+      final response = await chatGPT!.generateImage(request);
+      Vx.log(response!.data!.last!.url!);
+      insertNewData(response.data!.last!.url!, isImage: true);
+    } else {
+      final request =
+          CompleteText(prompt: message.text, model: kTranslateModelV3);
+
+      final response = await chatGPT!.onCompleteText(request: request);
+      Vx.log(response!.choices[0].text);
+      insertNewData(response.choices[0].text, isImage: false);
+    }
+  }
+
+  void insertNewData(String response, {bool isImage = false}) {
+    ChatMessage botMessage = ChatMessage(
+      text: response,
+      sender: "bot",
+      isImage: isImage,
+    );
+
+    setState(() {
+      _isTyping = false;
+      _messages.insert(0, botMessage);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -29,13 +88,27 @@ class _ChatScreenState extends State<ChatScreen> {
         Expanded(
           child: TextField(
             controller: _controller,
-            onSubmitted:  (value) => _sendMessage(),
-            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            onSubmitted: (value) => _sendMessage(),
+            decoration: const InputDecoration.collapsed(
+                hintText: "Question/description"),
           ),
         ),
-        IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: ()  => _sendMessage() ,
+        ButtonBar(
+          children: [
+            IconButton(
+              icon: const Icon(Icons.send),
+              onPressed: () {
+                _isImageSearch = false;
+                _sendMessage();
+              },
+            ),
+            TextButton(
+                onPressed: () {
+                  _isImageSearch = true;
+                  _sendMessage();
+                },
+                child: const Text("Generate Image"))
+          ],
         ),
       ],
     ).px16();
@@ -44,7 +117,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("ChatGPT Clone")),
+        appBar: AppBar(title: const Text("ChatGPT & Dall-E2 Demo")),
         body: SafeArea(
           child: Column(
             children: [
@@ -52,11 +125,15 @@ class _ChatScreenState extends State<ChatScreen> {
                   child: ListView.builder(
                 reverse: true,
                 padding: Vx.m8,
-                itemCount: _message.length,
+                itemCount: _messages.length,
                 itemBuilder: (context, index) {
-                  return _message[index];
+                  return _messages[index];
                 },
               )),
+              if (_isTyping) const ThreeDots(),
+              const Divider(
+                height: 1.0,
+              ),
               Container(
                 decoration: BoxDecoration(
                   color: context.cardColor,
